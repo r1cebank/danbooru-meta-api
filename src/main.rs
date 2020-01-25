05da612d4706a6f874a61492769a969479533df0
r1cebank <siyuangao@gmail.com>
2020-01-24 22:43:32 -0800

    Refactor to rocket

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,28 @@
+#![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use]
 extern crate diesel;
-#[macro_use]
 extern crate log;
-
-use actix_web::middleware::Logger;
-use actix_web::{App, HttpServer};
-use diesel::prelude::*;
-use diesel::r2d2::{self, ConnectionManager};
-use dotenv;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate rocket;
 
 mod models;
 mod routes;
 mod schema;
 mod util;
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "danbooru_meta_api=debug,actix_web=debug");
-    dotenv::dotenv().ok();
-    env_logger::init();
-    let connspec = std::env::var("DATABASE_URL").expect("DATABASE_URL");
-    let manager = ConnectionManager::<SqliteConnection>::new(connspec);
-    let pool = r2d2::Pool::builder()
-        .build(manager)
-        .expect("Failed to create pool.");
-    debug!("Starting server");
-    HttpServer::new(move || {
-        App::new()
-            .data(pool.clone())
-            .service(routes::index)
-            .service(routes::stat)
-            .service(routes::rand_posts)
-            .service(routes::tag_by_id)
-            .wrap(Logger::default())
-    })
-    .bind("0.0.0.0:3939")?
-    .run()
-    .await
+fn main() {
+    rocket::ignite()
+        .attach(models::MetadataDb::fairing())
+        .mount(
+            "/",
+            routes![
+                routes::index,
+                routes::stat,
+                routes::tag_by_id,
+                routes::rand_posts
+            ],
+        )
+        .launch();
 }
